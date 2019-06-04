6d92d857e4cf07af67eaa634b4ba0c65704a9d69
delaram-pouya <d.pouyabahar@gmail.com>
2019-06-05 02:57:50 +0430

    bug fixed

--- Codes/fastLA.R
+++ Codes/fastLA.R
@@ -23,7 +23,7 @@ indices <- which( colnames(GeneExp_merged.t) %in% topDiffExp_genes )
 result <- fastMLA(data=GeneExp_merged.t, topn=200, nvec=indices, rvalue=1.0, threads=detectCores()) 
 
 
-CNMcalc <- mass.CNM(data=data, GLA.mat=result, nback= 200)
+CNMcalc <- mass.CNM(data=GeneExp_merged.t, GLA.mat=result, nback= 200)
 finalres<-CNMcalc$"top p-values"
 write.csv(finalres,file="resfastLA_ecoli.csv")
 
@@ -32,7 +32,7 @@ boot <- CNMcalc$"bootstrap triplets"
 write.csv(boot,file="boot.csv")
 
 clust <- makeCluster(32)
-GLAeasy <- fastboots.GLA(boot, data=data, clust=clust, boots=30, perm=100, cut=3)
+GLAeasy <- fastboots.GLA(boot, data=GeneExp_merged.t, clust=clust, boots=30, perm=100, cut=3)
 stopCluster(clust)
 closeAllConnections()
 write.csv(GLAeasy,file=" resfasLAboot.csv")
